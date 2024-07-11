a8bd5c3619f11ced9cf611f54b54508813dbb340
pedroPecly <p.pecly@gsuite.iff.edu.br>
2024-07-11 19:33:45 -0300

    atualizando funcionalidades da dom e do localstorage

--- public/js/script.js
+++ public/js/script.js
@@ -1,110 +1,150 @@
-let btnAdd = document.getElementById("btnAdd");
-let inpDesc = document.getElementById("inpDesc");
+document.addEventListener('DOMContentLoaded', loadTasksFromLocalStorage);
+let btnAdd = document.getElementById('btn-add');
+let input = document.getElementById('desc');
+btnAdd.addEventListener('click', addTask);
 
-btnAdd.addEventListener("click", addTask);
-inpDesc.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        addTask();
-    }
+input.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Enter') addTask();
 });
 
-function getTarefas(){
-    return JSON.parse(localStorage.getItem("tarefas")) || [];
+function getTasksFromLocalStorage() {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
 }
 
-function salvarLocal(){
-    let tarefas = getTarefas();
+function setTasksToLocalStorage(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-function deleteItem(event) {
-    event.target.parentNode.remove();
+function loadTasksFromLocalStorage() {
+    let tasks = getTasksFromLocalStorage();
+    tasks.forEach((task) => {
+        let li = createListItem(task.desc);
+        if (task.concluida) {
+            formatDoneTask(li);
+            getDoneTasks().appendChild(li);
+
+        } else {
+            getTodoTasks().appendChild(li);
+        }
+    });
+    updateStatus(getTodoTasks(), getDoneTasks());
+}
+
+function deleteItem(evt) {
+    let tasks = getTasksFromLocalStorage();
+    //remove da dom
+    evt.target.parentNode.remove();
+    //remove da local storage
+    let desc = evt.target.previousSibling.textContent;
+    let index = getTasksIndex(tasks, desc);
+    tasks.splice(index, 1);
+    setTasksToLocalStorage(tasks);
+
     updateStatus(getTodoTasks(), getDoneTasks());
 }
 
 function updateStatus(todoList, doneList) {
-    let totalDone = doneList.childNodes.length
-    let totalTarefas = todoList.childNodes.length + totalDone;
-    let status = document.getElementById("status");
-    status.textContent = `${totalDone} of ${totalTarefas} done`;
+    let totalDone = doneList.childNodes.length;
+    let totalTasks = todoList.childNodes.length + totalDone;
+    let status = document.getElementById('status');
+    status.textContent = `${totalDone} of ${totalTasks} completed`;
 }
 
-function doneTask(event) {
-    let item = event.target.parentNode;
-    getDoneTasks().appendChild(event.target.parentNode);
-    //faz aparecer o botao de deletar
-    item.lastChild.classList.remove("hideElement");
-    //usa o mesmo css para fazer o checkbox sumir
-    item.firstChild.classList.add("hideElement");
-    item.classList.add("taskDone");
+function formatDoneTask(item) {
+    // mostra botão de exclusão
+    item.lastChild.classList.remove('hide-element');
+    // esconde checkbox
+    item.firstChild.classList.add('hide-element');
+    // formata descrição da tarefa
+    item.classList.add('task-done');
+}
+
+function getTasksIndex(tasks, desc){
+    return tasks.findIndex((task) => task.desc === desc);
+}
+
+function doneTask(evt) {
+    let item = evt.target.parentNode;
+    let desc = item.firstChild.nextSibling.textContent;
+
+    //atualiza CSS
+    formatDoneTask(item);
+    getDoneTasks().appendChild(item);
+
+
+    let tasks = getTasksFromLocalStorage();
+    let index = getTasksIndex(tasks, desc);
+    tasks[index].concluida = true;
+
+    setTasksToLocalStorage(tasks);
+
     updateStatus(getTodoTasks(), getDoneTasks());
 }
 
 function getTodoTasks() {
-    return document.getElementById("ulTarefas");
+    return document.getElementById('todo-tasks');
 }
 
 function getDoneTasks() {
-    return document.getElementById("ulFeitas");
+    return document.getElementById('done-tasks');
 }
 
 function createListItem(text) {
-    //criando o item da lista
-    let ul = document.createElement("li");
+    // criar o item da lista
+    let li = document.createElement('li');
     let textItem = document.createTextNode(text);
 
-    //criando a checkbox do item da lista
-    let check = document.createElement("input");
-    check.setAttribute("type", "checkbox");
-    check.addEventListener("click", doneTask)
+    // criação do checkbox
+    let check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+    check.addEventListener('click', doneTask);
 
-    //criando botao de deletar do item da lista
-    let btn = document.createElement("button");
-    let textBtn = document.createTextNode("deletar");
-    btn.addEventListener("click", deleteItem);
-    btn.classList.add("hideElement");
-    btn.appendChild(textBtn);
+    // criação do botão de exclusão
+    let btn = document.createElement('button');
+    let textBtn = document.createTextNode('del');
+    btn.addEventListener('click', deleteItem);
+    btn.classList.add('hide-element');
 
-    //adicionando todos os elementos ao item
-    ul.append(check, textItem, btn);
+    btn.append(textBtn);
+    li.append(check, textItem, btn);
 
-    return ul;
+    return li;
 }
 
 function inputReset(input) {
-    input.value = "";
-    input.focus();
+    input.value = '';
 }
 
 function itemExists(list, text) {
     let listItems = Array.from(list.childNodes);
 
-    let arrayResultado = listItems.filter((item) => {
+    let arrayResult = listItems.filter((item) => {
         return item.firstChild.nextSibling.textContent === text;
     });
-
-    return arrayResultado.length > 0;
+    return arrayResult.length;
 }
 
-function addTask(event) {
-    let inpDesc = document.getElementById("inpDesc");
-    let text = inpDesc.value.trim();
+function addTask() {
+    let input = document.getElementById('desc');
+    let text = input.value.trim();
+    let todoTasks = getTodoTasks();
 
-    //verificando se o elemento a ser adicionando nao vai ser uma string vazio ou varios espaços
-    if (text == "") {
-        alert("erro");
+    if (text === '') {
+        alert('Forneça uma descrição');
         return;
     }
 
-    if (itemExists(getTodoTasks(), text)) {
-        alert("item existente")
+    if (itemExists(todoTasks, text)) {
+        alert('Item já existe');
         return;
     }
 
-    //adicionando um item da lista
-    ulTarefas.appendChild(createListItem(text));
-    inputReset(inpDesc);
-    let tarefas = getTarefas();
-    tarefas.push({"desc": text, concluida: false});
-    localStorage.setItem("tarefas", JSON.stringify(tarefas));
-    updateStatus(getTodoTasks(), getDoneTasks());
+    // add o item na lista
+    todoTasks.appendChild(createListItem(text));
+    inputReset(input);
+
+    let tasks = getTasksFromLocalStorage();
+    tasks.push({ 'desc': text, 'concluida': false });
+    setTasksToLocalStorage(tasks);
+    updateStatus(todoTasks, getDoneTasks());
 }
